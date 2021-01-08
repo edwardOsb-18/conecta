5012a58a4024e6d36c2c4c10d2d17d6119da4b97
edward <edhuosb18mail.com>
2021-01-07 21:40:05 -0500

    ultima__prueba

--- docs/js/producto.js
+++ docs/js/producto.js
@@ -11,3 +11,21 @@ cancelar.addEventListener("click", () => {
     let bloque = document.getElementById("bloque")
     bloque.classList.remove("display__nuevo")
 })
+
+
+
+
+
+let icono__editar = document.getElementById("icono__editar")
+let cancelar__editar = document.getElementById("cancelar__editar")
+
+icono__editar.addEventListener("click", () => {
+
+    let bloque = document.getElementById("bloque__editar")
+    bloque.classList.add("display__nuevo")
+})
+
+cancelar__editar.addEventListener("click", () => {
+    let bloque = document.getElementById("bloque__editar")
+    bloque.classList.remove("display__nuevo")
+})
